b6ac3531c41d2b4f9a45a7f37ddb02e6224e16ee
Shalem Raju <shalemrajubiraja7@gmail.com>
2026-08-20 16:18:08 +0530

    changed BASE URL

--- frontend/src/services/careerService.js
+++ frontend/src/services/careerService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080/api";
+const BASE_URL = "https://career-graph-1.onrender.com/api";
 
 export const getCareerResults = (skills) => {
 
